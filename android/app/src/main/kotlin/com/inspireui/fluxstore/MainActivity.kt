defe87ea5253f7748e1a657e796e6b4fa10cecc9
meetSolo <meet.martin.solo@gmail.com>
2024-10-08 17:59:18 +0200

    settings

---
+++ android/app/src/main/kotlin/com/inspireui/fluxstore/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.inspireui.fluxstore
+
+import android.app.NotificationManager
+import android.content.Context
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.ryanheise.audioservice.AudioServiceFragmentActivity
+
+class MainActivity : AudioServiceFragmentActivity() {
+    override fun onResume() {
+        super.onResume()
+        closeAllNotifications();
+    }
+
+    private fun closeAllNotifications() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancelAll()
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
